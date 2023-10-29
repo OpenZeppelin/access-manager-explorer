92fcf130815b34ddf54732dccfb802bf1ad232a3
ernestognw <ernestognw@gmail.com>
2023-10-29 17:52:09 -0600

    Apply Empty network change logic in other empty states

--- src/components/entities/access-manager-role-member/empty/index.tsx
+++ src/components/entities/access-manager-role-member/empty/index.tsx
@@ -1,9 +1,20 @@
+import { useRouteNetwork } from "@/providers/route-network";
 import { InfoCircledIcon } from "@radix-ui/react-icons";
 import { Callout, Link } from "@radix-ui/themes";
 import { useChainModal } from "@rainbow-me/rainbowkit";
+import { switchNetwork } from "wagmi/actions";
 
 const Empty = () => {
   const { openChainModal } = useChainModal();
+  const { routeChainId, clientChainId } = useRouteNetwork();
+
+  const onChangeNetwork = () => {
+    if (routeChainId !== clientChainId) {
+      switchNetwork({ chainId: routeChainId });
+    } else {
+      openChainModal?.();
+    }
+  };
 
   return (
     <Callout.Root color="amber">
@@ -12,7 +23,7 @@ const Empty = () => {
       </Callout.Icon>
       <Callout.Text>
         Access Manager Role Member doesn&rsquo;t exist on this network.
-        <Link ml="1" onClick={openChainModal}>
+        <Link ml="1" onClick={onChangeNetwork}>
           Change network
         </Link>
       </Callout.Text>
